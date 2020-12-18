f3061ab9a2e101bf127ea3e438596b47209a3a3d
Luthfi Fitra Musyaffa <luthfimusafa@gmail.com>
2020-12-18 12:57:44 +0700

    implementasi another api

--- public/index.php
+++ public/index.php
@@ -90,13 +90,21 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
                         ],
                     ]);
                 }
-                else if($event['message']['text'] == '/positif')
-                {          
-                    $res = $bot->replyText($event['replyToken'], "Jumlah positif di Indonesia saat ini : " . $array[0][0]["positif"]);
+                else if($event['message']['text'] == '/onBoarding'){
+                    $res = $bot->replyText($event['replyToken'],
+                    " Aplikasi ini adalah aplikasi yang berguna untuk memantau perkembangan virus covid 19 atau dikenal
+                    sebagai corona di Indonesia, berikut beberapa hal yang bisa anda lakukan di bot ini : 
+                    - Lihat jumlah positif di Indonesia
+                    - Lihat jumlah pasien yang sudah sembuh di Indonesia
+                    - Lihat jumlah pasien yang sudah meninggal di Indonesia
+                    - Lihat perkembangan covid 19 di suatu provinsi
+                    - Lihat perkembangan covid 19 di suatu negara");
+                }else if($event['message']['text'] == '/positif'){          
+                    $res = $bot->replyText($event['replyToken'], "Jumlah pasien positif di Indonesia saat ini : " . $array[0][0]["positif"]);
                 }else if($event['message']['text'] == '/sembuh'){
-                    $res = $bot->replyText($event['replyToken'], "Jumlah positif di Indonesia saat ini : " . $array[0][0]["sembuh"]);
+                    $res = $bot->replyText($event['replyToken'], "Jumlah pasien sembuh di Indonesia saat ini : " . $array[0][0]["sembuh"]);
                 }else if($event['message']['text'] == '/meninggal'){
-                    $res = $bot->replyText($event['replyToken'], "Jumlah positif di Indonesia saat ini : " . $array[0][0]["meinggal"]);
+                    $res = $bot->replyText($event['replyToken'], "Jumlah pasien meninggal di Indonesia saat ini : " . $array[0][0]["meninggal"]);
                 }
                 else{
                     $flexTemplate = file_get_contents("../flexMessageGroup.json"); // template flex message
